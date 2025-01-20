a3a9039cec035e8e99cb032b5bd6e60e464abe7a
Ceej <CJBowman@live.com>
2025-01-20 12:31:01 -0700

    Adding moderation features

--- obj/Debug/net8.0/GKBInternational.AssemblyInfo.cs
+++ obj/Debug/net8.0/GKBInternational.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GKBInternational")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a4016eaf159fba488927ab02547a69393d60cb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57f578c2fa149858b688dae54fc41ea849f73b0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GKBInternational")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GKBInternational")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
