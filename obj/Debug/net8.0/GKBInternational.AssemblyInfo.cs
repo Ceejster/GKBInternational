aea188e1497e5961d1b94f4daf5d20e494353274
Ceej <CJBowman@live.com>
2025-01-26 05:08:29 -0700

    fixing

--- obj/Debug/net8.0/GKBInternational.AssemblyInfo.cs
+++ obj/Debug/net8.0/GKBInternational.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GKBInternational")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39b4cf086cf79f8b738d607012a3374ba21747da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+010f2be5f0b339baebab94e2cc2e768781974f4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GKBInternational")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GKBInternational")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
